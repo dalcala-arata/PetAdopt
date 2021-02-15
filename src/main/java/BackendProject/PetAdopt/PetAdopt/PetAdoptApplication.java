60be3e9099a1fe2be69a1af6a2cee06b373c6ec4
Diego Alcalá <dalcala.arata@gmail.com>
2021-02-14 20:12:00 -0500

    Commit inicial

---
+++ src/main/java/BackendProject/PetAdopt/PetAdopt/PetAdoptApplication.java
@@ -0,0 +1,13 @@
+package BackendProject.PetAdopt.PetAdopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BackendProject/PetAdopt/PetAdopt/PetAdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package BackendProject.PetAdopt.PetAdopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
